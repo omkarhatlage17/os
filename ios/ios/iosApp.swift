ff0b9ac9a0f86c407c0216ee6de411671b467f5e
Akash Raut <akashraut@itadmins-Mac-mini.local>
2023-04-04 16:21:03 +0530

    ios_build

---
+++ ios/ios/iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  iosApp.swift
+//  ios
+//
+//  Created by Akash Raut on 04/04/23.
+//
+
+import SwiftUI
+
+@main
+struct iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
